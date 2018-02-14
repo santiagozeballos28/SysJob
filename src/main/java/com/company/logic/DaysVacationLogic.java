32b0118450041f45c11f2ccd86056ed6a80a706d
Santiago Mamani <santiago.mamani@bolivia.assuresoft.com>
2018-02-14 19:29:53 -0400

    commit

---
+++ src/main/java/com/company/logic/DaysVacationLogic.java
@@ -0,0 +1,14 @@
+package com.company.logic;
+
+import com.company.util.ObjectResponce;
+
+/**
+ *
+ * @author santiago.mamani
+ */
+public class DaysVacationLogic {
+
+    public ObjectResponce fillVacationDays() {
+   return null;
+    }
+}
